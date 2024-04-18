d7ed082ac00e3dc1a481fdc6703e92a7b32fdd4a
Mouhamad-Shaker-1 <mouhamadshaker073@gmail.com>
2024-04-18 15:32:51 -0500

    add data render to projectDetail page

--- src/pages/projects/ProjectDetail.jsx
+++ src/pages/projects/ProjectDetail.jsx
@@ -4,8 +4,10 @@ import { faHtml5 } from '@fortawesome/free-brands-svg-icons'
 import { faCss3Alt } from '@fortawesome/free-brands-svg-icons'
 import { faJs } from '@fortawesome/free-brands-svg-icons'
 import { faReact } from '@fortawesome/free-brands-svg-icons'
-import { connectFunctionsEmulator } from 'firebase/functions'
-import { useLoaderData, useParams } from 'react-router-dom'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+import { useLoaderData, useParams, Link } from 'react-router-dom'
 
 import { getProject } from '../../api'
 
@@ -17,53 +19,90 @@ export async function loader({ params }) {
 export default function ProjectDetail() {
 
     const projectData = useLoaderData()
+
+    const languages = getValuesFromObject(projectData.languages)
+    const images = getValuesFromObject(projectData.imagesName)
+    console.log(projectData)
+
+    const imgsProject = images.map(img => {
+        return (
+            <img className='imgs-project' src={`/img/projects-img/${projectData.name}/${img}.png`}/>
+        )
+    })
+
+    const iconProject = languages.map(lang => {
+        if (lang == 'HTML') {
+            return (
+                <div className="lang">
+                    <FontAwesomeIcon key={lang} className='icon' icon={faHtml5} />
+                    <p>{ lang }</p>
+                </div>
+            )
+        } else if (lang == 'CSS') {
+            return (
+                <div className="lang">
+                    <FontAwesomeIcon key={lang} className='icon' icon={faCss3Alt} />
+                    <p>{ lang }</p>
+                </div>
+            )
+        } else if (lang == "javascript") {
+            return (
+                <div className="lang">
+                    <FontAwesomeIcon key={lang} className='icon' icon={faJs} />
+                    <p>{ lang }</p>
+                </div>
+            )
+        } else if (lang == "react") {
+            return (
+                <div className="lang">
+                    <FontAwesomeIcon key={lang} className='icon' icon={faReact} />
+                    <p>{ lang }</p>
+                </div>
+            )
+        }
+    })
  
     return (
         <section className='section-project-detail'>
-            <h1>Quiz app</h1>
+            <h1>{ projectData.name }</h1>
             <hr />
             <div className="container-lang">
+                <Link to={ projectData.link }>
+                    <div className="lang">
+                        <FontAwesomeIcon className='icon' icon={faCode} />
+                        <p>Demo</p>
+                    </div>
+                </Link>
+                
                 <div className="lang">
-                    <FontAwesomeIcon className='icon' icon={faHtml5} />
-                    <p>htasdfasdfml</p>
+                    <FontAwesomeIcon className='icon' icon={faGithub} />
+                    <p>Gethub</p>
                 </div>
-                <div className="lang">
-                    <FontAwesomeIcon className='icon' icon={faHtml5} />
-                    <p>html</p>
-                </div>
-                <div className="lang">
-                    <FontAwesomeIcon className='icon' icon={faHtml5} />
-                    <p>sdfsdfsdf</p>
-                </div>
-                <div className="lang">
-                    <FontAwesomeIcon className='icon' icon={faHtml5} />
-                    <p>html</p>
-                </div>
-                <div className="lang">
-                    <FontAwesomeIcon className='icon' icon={faHtml5} />
-                    <p>html</p>
-                </div>
-                <div className="lang">
-                    <FontAwesomeIcon className='icon' icon={faHtml5} />
-                    <p>html</p>
-                </div>
+
+                {iconProject}
             </div>
             <hr />
-            <p className="description">
-                it esay to deverlop like this app and i so happy
-                to learn proggraming its easy and so much fun 
-                this is the description i want to take
-                about this app but i dont have any idea to take about so let fell this description by some
-                text to test the css ok i was so bored yesterday but now i am so happy and 
-                very exiated about the day its such a beuatifull day i need one word
-            </p>
+            <p className="description">{ projectData.description }</p>
             <hr />
 
             <div className="container-imgs-project">
-                {/* <img className='imgs-project' src={imgs} />
-                <img className='imgs-project' src={imgs} />
-                <img className='imgs-project' src={imgs} /> */}
+                {imgsProject}
             </div>
         </section>
     )
+}
+
+function convertDateToStringFromFireStore(dateStore) {
+    const date = dateStore.toDate()
+    return date.toISOString().substring(0, 10).replace(/-/g, '/');
+}
+
+function getValuesFromObject(objs) {
+    var vals = [];
+    for( var key in objs ) {
+        if ( objs.hasOwnProperty(key) ) {
+            vals.push(objs[key]);
+        }
+    }
+    return vals;
 }
